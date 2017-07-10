26bfe618126c1358d8f93a18a3d92048e2331cb9
shortcircuit1024 <shortcircuit1024@gmail.com>
2017-07-09 23:46:27 -0500

    updates to login and signup

--- imports/ui/login.js
+++ imports/ui/login.js
@@ -31,32 +31,36 @@ class Login extends Component {
   render() {
     if (!Meteor.userId()) {
       return (
-        <div className="container-fluid row">
-          <div className="col-8 offset-2">
+        <div className="row">
+          <div className="col-10 offset-1">
             <h1 className="display-4 alert alert-danger text-center" style={{ fontFamily: 'Electrolize' }}>
-              <span className="fa fa-exclamation-triangle" aria-hidden="true"></span> Restricted Access <span className="fa fa-exclamation-triangle" aria-hidden="true"></span>
+              <span className="fa fa-exclamation-triangle" aria-hidden="true"></span>
+               Restricted Access 
+              <span className="fa fa-exclamation-triangle" aria-hidden="true"></span>
             </h1>
             <div className="card text-center row ">
               <h2 className="card-header" style={{ fontFamily: 'Electrolize' }}><strong> <span style={{ fontSize: '150%' }}>A</span>rmitage <span style={{ fontSize: '150%' }}>M</span>obile <span style={{ fontSize: '150%' }}>M</span>ilitia <span style={{ fontSize: '150%' }}>O</span>ffensive Usage Only</strong></h2>
+              <br/>
               <h4 className="card-title" style={{fontFamily: 'Electrolize'}}>A.M.M.O. Login</h4>
               <p className="card-text">For authorized users, Welcome. Please enter your Credentials</p>
               <form onSubmit={this.onSubmit.bind(this)}>
-                <div className="input-group container-fluid col-4 offset-4 row">
-                  <div className="input-group-addon container-fluid col-3">Email</div>
+                <div className="input-group container-fluid col-6 offset-3 row">
+                  <div className="input-group-addon container-fluid col-3 text-right" style={{width: '100px'}}>Email</div>
                   <input type="text" className="form-control container-fluid col-9" ref="email"/>
                 </div>
                 <br/>
-                <div className="input-group col-4 offset-4 row">
-                  <div className="input-group-addon col-3">Password</div>
+                <div className="input-group col-6 offset-3 row">
+                  <div className="input-group-addon col-3 text-right" style={{width: '100px'}}>Password</div>
                   <input type="password" className="form-control col-9" ref="password"/>
                 </div>
                 <br/>
-                <div className="col-4 offset-4 row">
+                <div className="col-6 offset-3 row container-fluid">
                   <button type="submit" className="btn btn-primary btn-block">Submit</button>
                 </div>
                 <br/>
               </form>
               <Link to="/signup" className="text-center">Need a Account?</Link>
+              <br/>
               <div className="card-footer text-muted">
                 <ClockUTC/>
               </div>
--- imports/ui/signup.js
+++ imports/ui/signup.js
@@ -10,7 +10,8 @@ class Signup extends Component {
     super(props);
     this.state = {
       error: '',
-      branch: ''
+      branch: '',
+      passwordPrompt: false
     }
   }
   componentWillMount(){
@@ -60,7 +61,6 @@ class Signup extends Component {
   handleBranchClick = (value) => {
     this.setState({branch: value})
   }
-
   render() {
       return (
         <div className="container-fluid">
@@ -75,19 +75,19 @@ class Signup extends Component {
                   <h6 className="card-subtitle mb-2 text-muted">Username, Password, and Email</h6>
                   <div className="input-group">
                     <div className="input-group-addon text-right" style={{width: '170px'}}>Username:</div>
-                    <input type="text" ref="username" className="form-control" placeholder="" />
+                    <input type="text" ref="username" className="form-control" placeholder="Please use your in-game handle" />
                   </div>
                   <br/>
                   <div className="input-group">
-                    <div className="input-group-addon text-right" style={{width: '170px'}}>Password:</div>
+                    <div className="input-group-addon text-right" style={{width: '170px'}} onChange={() => {console.log(this.refs)}}>Password:</div>
                     <input type="password" ref="password" className="form-control"/>
                   </div>
-                  <br/>
+                  {this.state.passwordPrompt ? <p className="card-text-muted text-right">Must be over 8 characters</p> : <br/>}
                   <div className="input-group">
                     <div className="input-group-addon text-right" style={{width: '170px'}}>Confirm Password:</div>
-                    <input type="password" ref="confirmPassword" className="form-control"/>
+                    <input type="password" ref="confirmPassword" className="form-control" value=""/>
                   </div>
-                  <br/>
+                  {this.state.passwordPrompt ? <p className="card-text-muted text-right">Must be over 8 characters</p> : <br/>}
                   <div className="input-group">
                     <div className="input-group-addon text-right" style={{width: '170px'}}>Email:</div>
                     <input type="text" ref="email" className="form-control" placeholder="example@example.com" />
@@ -95,7 +95,7 @@ class Signup extends Component {
                   <br/>
                   <div className="input-group">
                     <div className="input-group-addon text-right" style={{width: '170px'}}>Confirm Email:</div>
-                    <input type="text" ref="confirmEmail" className="form-control"/>
+                    <input type="text" ref="confirmEmail" className="form-control" placeholder="example@example.com"/>
                   </div>
                   <br/>
                 </div>
@@ -103,7 +103,31 @@ class Signup extends Component {
               </div>
             </div>
             <br/>
-            <button className="btn btn-block">Submit</button>
+            <div className="row">
+              <div className="card card-outline-primary col-12">
+                <div className="card-block">
+                  <h4 className="display-4">Citizen Info</h4>
+                  <h6 className="card-subtitle mb-2 text-muted">Branch, Primary Trade, and Secondary Trade</h6>
+                  <br/>
+                  <h4>Branch</h4>
+                  <div className="btn-group" data-toggle="buttons">
+                    <label className={(this.state.branch === 'Fleet') ? 'btn btn-primary active' : 'btn btn-primary'}>
+                      <input type="radio" id="fleetButton" autoComplete="off" onClick={() => this.setState({ branch: 'Fleet'})} />Fleet
+                    </label>
+                    <label className={(this.state.branch === 'Marines') ? 'btn btn-primary active' : 'btn btn-primary'}>
+                      <input type="radio" id="marinesButton" autoComplete="off" onClick={() => this.setState({ branch: 'Marines'})} />Marines
+                    </label>
+                    <label className={(this.state.branch === 'PaIR') ? 'btn btn-primary active' : 'btn btn-primary'}>
+                      <input type="radio" id="pairButton" autoComplete="off" onClick={() => this.setState({ branch: 'PaIR'})} />Public and Internal Relations
+                    </label>
+                    <label className={(this.state.branch === 'LaS') ? 'btn btn-primary active' : 'btn btn-primary'}>
+                      <input type="radio" id="lasButton" autoComplete="off" onClick={() => this.setState({ branch: 'LaS'})} />Logistics and Science
+                    </label>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <button className="btn btn-block" style={{margin: '0px'}}>Submit</button>
           </form>
         </div>
       )
