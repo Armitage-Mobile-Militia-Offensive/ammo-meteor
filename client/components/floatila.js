816b73617fd55b0a44850793be3f2da11f9ba059
shortcircuit1024 <shortcircuit1024@gmail.com>
2017-07-25 13:11:02 -0500

    Structure updates
    Updates to WIP screens for all pages and outline of database objects

--- client/components/floatila.js
+++ client/components/floatila.js
@@ -8,7 +8,7 @@ const Floatila = () => {
       </div>
       <div className="">
         <p className="text-center display-4"><span style={{color: '#F59B45'}}>A.M.M.O.</span> Collective Fleet Ships</p>
-        <p className="text-muted text-center">Under contruction</p>
+        <p className="text-muted text-center h3"><i className="fa fa-code" aria-hidden="true"></i> Under contruction <i className="fa fa-code" aria-hidden="true"></i></p>
       </div>
     </div>
   );
---
+++ imports/api/matrixShips.js
@@ -0,0 +1,38 @@
+/*
+  Matrix Template
+    _id
+    Manufacturer
+    Hull
+    Variant
+    minCrew
+    maxCrew
+    cargo
+    quantumFuel
+    shipSize
+    lti
+    role
+  Fleet Template
+    _id
+    crossShipID
+    jobs []
+    openPositionsNum
+    openPositions []
+    captainID
+    ownerID
+    exoID
+    assignments []
+    base
+    fleetGroup
+  Floatila Template
+    _id
+    crossShipID
+    jobs []
+    openPositionsNum
+    openPositions []
+    captainID
+    ownerID
+    exoID
+    assignments []
+    base
+    militiaGroup
+*/
