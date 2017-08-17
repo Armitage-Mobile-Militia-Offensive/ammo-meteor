e2165e53a816c8de29195822ab9864215aa44d1d
Short_Circuit <shortcircuit1024@gmail.com>
2017-08-17 12:47:33 -0500

    Updates

--- client/components/fleet.js
+++ client/components/fleet.js
@@ -3,13 +3,8 @@ import React from 'react';
 const Fleet = () => {
   return (
       <div className="container">
-        <div className="text-center display-4">
-          <span className="fa fa-calendar-o" aria-hidden="true"></span>
-        </div>
-        <div className="">
           <p className="text-center display-4"><span style={{color: '#F59B45'}}>A.M.M.O.</span> Offical Fleet</p>
           <p className="text-muted text-center h3"><i className="fa fa-code" aria-hidden="true"></i> Under contruction <i className="fa fa-code" aria-hidden="true"></i></p>
-        </div>
       </div>
   );
 };
--- client/components/schedule.js
+++ client/components/schedule.js
@@ -3,7 +3,6 @@ import React from 'react';
 const Schedule = () => {
   return (
         <div className="container">
-          <div className="display-4"><span className="fa fa-calendar-o" aria-hidden="true"></span></div>
           <p className="text-center display-4"><span style={{color: '#F59B45'}}>A.M.M.O.</span> Event Scheduler</p>
           <p className="text-muted text-center h3"><i className="fa fa-code" aria-hidden="true"></i> Under contruction <i className="fa fa-code" aria-hidden="true"></i></p>
         </div>
