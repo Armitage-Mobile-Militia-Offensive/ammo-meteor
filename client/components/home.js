5120c9fb57af688d0403833ce3afc2889fd99eb7
shortcircuit1024 <shortcircuit1024@gmail.com>
2017-09-04 16:08:36 -0500

    Home style updates

--- client/components/home.js
+++ client/components/home.js
@@ -11,14 +11,12 @@ const Home = () => {
       backgroundSize: 'cover',
       backgroundPosition: 'center'
     }}>
-      <div className="d-flex align-items-center justify-content-center" >
-        <div className="row">
-              <div className="text-center mw-100" style={{ fontFamily: 'Electrolize', color: 'white'}}>Armitage Mobile Militia Offensive</div>
-              <div className="text-center display-1 mw-100" style={{ fontSize: '5rem', fontFamily: 'Electrolize', marginBottom: '0%', color: '#F59B45'}}><b> A . M . M . O . </b></div>
-              <div className="text-center text-muted mw-100" style={{ fontFamily: 'Offside', color: 'white'}}><small>ex oblivione, unitas</small></div>
-        </div>
+      <div className="d-flex align-items-center">
+              <div className="text-center " style={{ fontFamily: 'Electrolize', color: 'white'}}>Armitage Mobile Militia Offensive</div>
+              <div className="text-center display-1 " style={{ fontSize: '5rem', fontFamily: 'Electrolize', marginBottom: '0%', color: '#F59B45'}}><b> A . M . M . O . </b></div>
+              <div className="text-center text-muted " style={{ fontFamily: 'Offside', color: 'white'}}><small>ex oblivione, unitas</small></div>
       </div>
-      <div className="d-flex align-items-end justify-content-center" style={{color: 'white', fontFamily: 'Electrolize'}}>Join our discord to data at <a style={{color: '#F59B45'}} className="nav-text" href="https://discord.gg/pgJs3u6">here</a> or our RSI page at <a style={{color: '#F59B45'}} className="nav-text" href="https://robertsspaceindustries.com/orgs/AMMO">{'https://robertsspaceindustries.com/orgs/AMMO'}</a></div>
+      <div className="d-flex align-items-end text-center" style={{color: 'white', fontFamily: 'Electrolize', width: '100vw'}}>Join our discord to data at <a style={{color: '#F59B45', paddingLeft: '5px', paddingRight: '5px'}} className="nav-text" href="https://discord.gg/pgJs3u6"> here </a> or our RSI page at <a style={{color: '#F59B45', paddingLeft: '5px'}} className="nav-text" href="https://robertsspaceindustries.com/orgs/AMMO">{'https://robertsspaceindustries.com/orgs/AMMO'}</a></div>
     </div>
   )
 }
